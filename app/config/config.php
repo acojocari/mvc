4ca6fb6fcd50ab78158df779e7ed36fda3f2c39b
acojocari <anatolcojocari@gmail.com>
2020-03-27 12:45:29 +0100

    PDO Database Class & View

--- app/config/config.php
+++ app/config/config.php
@@ -8,5 +8,5 @@ define("SITENAME",'MVC');
 // DB Params
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','123456');
+define('DB_PASS','');
 define('DB_NAME','tMVC');
--- app/models/Post.php
+++ app/models/Post.php
@@ -6,4 +6,10 @@ class Post{
     {
         $this->db = new Database;
     }
+    public function getPosts(){
+        $this->db->query("SELECT * FROM posts;");
+
+        return $this->db->resultSet();
+    }
+
 }
