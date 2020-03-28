7adc3196ccbb5c5c350f8abe3f56dc69eb7f6be2
acojocari <anatolcojocari@gmail.com>
2020-03-28 10:55:52 +0100

    Form validation

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -8,7 +8,59 @@ class Users extends Controller {
     public function register(){
      //check for POST
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            //process form
+            //Process form
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+            //Init data
+            $data=[
+                'name'=>trim($_POST['name']),
+                'email'=>trim($_POST['email']),
+                'password'=>trim($_POST['password']),
+                'confirm_password'=>trim($_POST['confirm_password']),
+                'name_err'=>'',
+                'email_err'=>'',
+                'password_err'=>'',
+                'confirm_password_err'=>''
+            ];
+
+            //Validate Email
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter email';
+            }
+
+            //Validate Name
+            if(empty($data['name'])){
+                $data['name_err'] = 'Please enter name';
+            }
+
+            //Validate Password
+            if(empty($data['password'])){
+                $data['password_err'] = 'Please enter password';
+            }elseif (strlen($data['password'] < 6)){
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+            //Validate Confirm password
+            if(empty($data['confirm_password'])){
+                $data['confirm_password_err'] = 'Please confirm password';
+            }else{
+                if($data['password'] != $data['confirm_password']){
+                    $data['confirm_password_err'] = 'Password do not match';
+                }
+            }
+
+            //Make sure that those errors are empty
+            if(empty($data['email_err'])
+                && empty($data['name_err'])
+                && empty($data['password_err'])
+                && empty($data['confirm_password_err'])){
+                //Validated
+                die('SUCCESS');
+            }else{
+                //Load view with errors
+                $this->view('users/register',$data);
+            }
+
      }else
         {
             //load de form
@@ -33,6 +85,36 @@ class Users extends Controller {
         //check for POST
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             //process form
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+            //Init data
+            $data=[
+                'email'=>trim($_POST['email']),
+                'password'=>trim($_POST['password']),
+                'email_err'=>'',
+                'password_err'=>'',
+            ];
+
+            //Validate Email
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter email';
+            }
+
+            //Validate Password
+            if(empty($data['email'])){
+                $data['password_err'] = 'Please enter password';
+            }
+
+            //Make sure that those errors are empty
+            if(empty($data['email_err'])
+                && empty($data['password_err'])){
+                //Validated
+                die('SUCCESS');
+            }else{
+                //Load view with errors
+                $this->view('users/login',$data);
+            }
+
         }else
         {
             //load de form
